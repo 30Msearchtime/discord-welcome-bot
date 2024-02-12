2cdbc1f0e001e8a02ed2662064c97641c2cbdc43
Tim <94912397+30Msearchtime@users.noreply.github.com>
2024-02-12 13:01:19 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+module.exports = {
+  token: 'YOUR_BOT_TOKEN',
+  prefix: '!',
+  welcomeChannelId: 'WELCOME_CHANNEL_ID',
+  leaveChannelId: 'LEAVE_CHANNEL_ID',
+  welcomeMessage: 'Welcome to the server, {user}!',
+  leaveMessage: 'Sorry to see you go, {user}!',
+  bannerUrl: 'YOUR_BANNER_URL',
+  iconUrl: 'YOUR_ICON_URL'
+};
