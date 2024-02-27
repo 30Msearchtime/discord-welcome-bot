3e6aa23467dc757fcefca43acd9d56e7e33c43eb
Tim <94912397+30Msearchtime@users.noreply.github.com>
2024-02-27 11:47:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 // config.js
 module.exports = {
   token: 'YOUR_BOT_TOKEN',
-  prefix: '!',
   welcomeChannelId: 'WELCOME_CHANNEL_ID',
   leaveChannelId: 'LEAVE_CHANNEL_ID',
   welcomeMessage: 'Welcome to the server, {user}!',
+  dmWelcomeMessage: 'Hello {user}, welcome to our Discord server!',
   leaveMessage: 'Sorry to see you go, {user}!',
-  dmWelcomeMessage: 'Welcome to the server, {user}!', // 
-  dmLeaveMessage: 'Sorry to see you go, {user}!',
+  dmLeaveMessage: 'It's sad to see you leave, {user}. Hope to see you back soon!',
   bannerUrl: '', // YOUR_BANNER_URL
   iconUrl: '' // YOUR_ICON_URL
 };
